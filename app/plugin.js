04f2af4d9abfe48032b608eec38a102ea79fbb5b
joubn <595700239@qq.com>
2016-09-16 15:41:38 +0800

    修改

--- app/plugin.js
+++ app/plugin.js
@@ -1,6 +1,6 @@
 //some shitty jquery plugin...
 (function ( $ ) {
-    const shade = "#556b2f";
+    const shade = "#556b2c";
     $.fn.greenify = function() {
         this.css( "color", shade );
         return this;
